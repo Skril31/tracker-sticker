4b2de21ba186168f8fe6e02d084ad89cc1b0d244
Skril31 <82641768+Skril31@users.noreply.github.com>
2021-06-24 00:29:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector('.bi-list')
+let menuList = document.querySelector('.header_wrapper')
+menuBtn.onclick = function() {
+    menuList.classList.toggle('show')
+}
+
